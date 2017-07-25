48a6ccea03f06c47b6817244de49b8207bb61108
James W Moody <jamesmoodyradio@gmail.com>
2017-07-25 15:23:07 -0500

    Complete markdown syntax converter

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ var MarkdownWidget = function(textarea, preview) {
     var toPreview = document.getElementById(preview.substring(1))
 
     var formattedInput = input.replace(/(?:\r\n|\r|\n)/g, '<br />')
-                          .replace("**", "<b>")
-                          .replace("*", "<em>")
-                          .replace("_", "<em>");
+                          .replace(/[*]{2}(.*?)[*]{2}/g, '$1'.bold())
+                          .replace(/[*](.*?)[*]/, '$1'.italics())
+                          .replace(/[_](.*?)[_]/, '$1'.italics());
 
     toPreview.innerHTML = "";
     toPreview.innerHTML = toPreview.innerHTML + formattedInput;
