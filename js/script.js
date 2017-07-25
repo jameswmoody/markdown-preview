d740c9547182bcf2638cd7622d5f587c905290a5
James W Moody <jamesmoodyradio@gmail.com>
2017-07-25 13:25:14 -0500

    Clean up script.js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,17 @@
 var MarkdownWidget = function(textarea, preview) {
-  // this.preview = document.getElementById(preview.substring(1));
-
   insertIntoPreview = function() {
-
 
     var input = document.getElementById(textarea.substring(1)).value
     var toPreview = document.getElementById(preview.substring(1))
 
-    var boldToHTML = input.replace("**", "<b>");
-    var italicsToHTMLOne = boldToHTML.replace("*", "<em>");
-    var italicsToHTMLTwo = italicsToHTMLOne.replace("_", "<em>");
-    var formattedInput = italicsToHTMLTwo
+    var formattedInput = input.replace(/(?:\r\n|\r|\n)/g, '<br />')
+                          .replace("**", "<b>")
+                          .replace("*", "<em>")
+                          .replace("_", "<em>");
 
     toPreview.innerHTML = "";
     toPreview.innerHTML = toPreview.innerHTML + formattedInput;
-    console.log(input);
   };
-
 };
 
-
 var widget = new MarkdownWidget("#source-id", "#preview-div")
-
-// $(document).ready(function() {
-//   var test = 1
-//
-//   var insertIntoPreview = function() {
-//     test++
-//     console.log(test);
-//   };
-// });
